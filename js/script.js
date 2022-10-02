f5821f676aa01a64f7dfaf49f9c06a455e81acbd
Raigardas <39338097+betadeveloper@users.noreply.github.com>
2022-10-02 23:52:09 +0300

    big changes to website
    added login page, extra projects

--- js/script.js
+++ js/script.js
@@ -36,10 +36,12 @@ function closeModal(modal) {
   overlay.classList.remove("active");
 }
 
+
+
 function clickedProject(event) {
   switch (event.target.id) {
     case "firstProj":
-      modalImg.src = "/images/SuppleSport.PNG";
+      modalImg.src = "/images/SuppleSport.webp";
       modalHeading.innerText = "Supplement Landing Page";
       modalText.innerText =
         "Front-End for a landing page for protein supplements with dynamically changing prices according to selection using JavaScript. Made using TailWindCSS. Highlights Front-End and design skills.";
@@ -48,7 +50,7 @@ function clickedProject(event) {
       break;
 
     case "secondProj":
-      modalImg.src = "/images/WeatherAppIMG.PNG";
+      modalImg.src = "/images/WeatherAppIMG.webp";
       modalHeading.innerText = "Weather App using OWM API";
       modalText.innerText =
         "A Weather app that takes your current location and displays the weather that it retrieves from the Open Weather Map API. Includes a search bar to search for a city in the world and get weather data from it. Clicking the temperature displays the fahrenheit equivalent.";
@@ -57,7 +59,7 @@ function clickedProject(event) {
       break;
 
     case "thirdProj":
-      modalImg.src = "/images/photogen.PNG";
+      modalImg.src = "/images/photogen.webp";
       modalHeading.innerText = "Photo Generator using Pexels API";
       modalText.innerText =
         "A photo generator which takes data from the Pexels API and returns result to load images on demand. This project works with APIs, asynchronous JavaScript, and JavaScript promises";
@@ -65,7 +67,7 @@ function clickedProject(event) {
       sourceBtn.href = "https://github.com/betadeveloper/PhotoGenerator";
       break;
     case "fourthProj":
-      modalImg.src = "/images/portfolioIMG.PNG";
+      modalImg.src = "/images/portfolioIMG.webp";
       modalHeading.innerText = "Portfolio Website";
       modalText.innerText =
         "Personal portfolio, fully created and designed by me using vanilla JavaScript, HTML5, CSS3 and Swirl.JS for the effects on the page. ";
@@ -73,7 +75,7 @@ function clickedProject(event) {
       sourceBtn.href = "https://github.com/betadeveloper/portfolio";
       break;
     case "fifthProj":
-      modalImg.src = "/images/CookieAlertIMG.PNG";
+      modalImg.src = "/images/CookieAlertIMG.webp";
       modalHeading.innerText = "Cookie Alert";
       modalText.innerText =
         "A website I created that displays a cookie notification for the user. The user can either accept or decline the cookies and their preference is saved in the local storage. Useful for websites targeted towards EU citizens.";
@@ -81,13 +83,21 @@ function clickedProject(event) {
       sourceBtn.href = "https://github.com/betadeveloper/CookieNotification";
       break;
     case "sixthProj":
-      modalImg.src = "/images/ColorGenIMG.PNG";
+      modalImg.src = "/images/ColorGenIMG.webp";
       modalHeading.innerText = "Color Palette Generator";
       modalText.innerText =
         "A web app that creates a random color palette that can be saved, and the palette gets stored in the local storage. Project created using HTML, CSS, JavaScript, jQuery and some SASS.";
       previewBtn.href = "/ColorGen/";
       sourceBtn.href = "https://github.com/betadeveloper/ColorPalleteGenerator";
-  }
+      break;
+  
+  case "comingSoon": 
+  modalImg.src = "";
+  modalHeading.innerText = "Project coming soon!";
+  modalText.innerText = "Coming soon";
+  previewBtn.href ="";
+  sourceBtn.href = "";
+}
 }
 
 let nav = document.querySelector("nav");
